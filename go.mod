9abb9511471c2d78b72debc687030fd7eda058da
Kyosu-K <1406kimura@gmail.com>
2023-03-18 07:40:54 +0000

    add 環境変数の読み込み追加

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/Kyosu-K/go_todo_app
 go 1.19
 
 require golang.org/x/sync v0.1.0
+
+require github.com/caarlos0/env/v6 v6.10.1 // indirect
--- main.go
+++ main.go
@@ -8,26 +8,28 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/Kyosu-K/go_todo_app/config"
 	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Printf("need port number\n")
-		os.Exit(1)
-	}
-	p := os.Args[1]
-	l, err := net.Listen("tcp", ":"+p)
-	if err != nil {
-		log.Fatalf("failed to listen port %s: %v", p, err)
-	}
-	if err := run(context.Background(), l); err != nil {
+	if err := run(context.Background()); err != nil {
 		log.Printf("failed to terminate server: %v", err)
 		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context, l net.Listener) error {
+func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
 	s := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
