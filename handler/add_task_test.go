8e1e56f1144f11d8638df658635011d5ffb6d50c
Kyosu-K <1406kimura@gmail.com>
2023-03-25 08:08:02 +0000

    add タスク登録のテスト追加

---
+++ handler/add_task_test.go
@@ -0,0 +1,66 @@
+package handler
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Kyosu-K/go_todo_app/entity"
+	"github.com/Kyosu-K/go_todo_app/store"
+	"github.com/Kyosu-K/go_todo_app/testutil"
+	"github.com/go-playground/validator/v10"
+)
+
+func TestAddTask(t *testing.T) {
+	t.Parallel()
+	type want struct {
+		status  int
+		rspFile string
+	}
+	tests := map[string]struct {
+		reqFile string
+		want    want
+	}{
+		"ok": {
+			reqFile: "testdata/add_task/ok_req.json.golden",
+			want: want{
+				status:  http.StatusOK,
+				rspFile: "testdata/add_task/ok_rsp.json.golden",
+			},
+		},
+		"badRequest": {
+			reqFile: "testdata/add_task/bad_req.json.golden",
+			want: want{
+				status:  http.StatusBadRequest,
+				rspFile: "testdata/add_task/bad_rsp.json.golden",
+			},
+		},
+	}
+
+	for n, tt := range tests {
+		tt := tt
+		t.Run(n, func(t *testing.T) {
+			t.Parallel()
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(
+				http.MethodPost,
+				"/tasks",
+				bytes.NewReader(testutil.LoadFile(t, tt.reqFile)),
+			)
+
+			sut := AddTask{
+				Store: &store.TaskStore{
+					Tasks: map[entity.TaskID]*entity.Task{},
+				},
+				Validator: validator.New(),
+			}
+			sut.ServeHTTP(w, r)
+
+			resp := w.Result()
+			testutil.AssertResponse(t,
+				resp, tt.want.status, testutil.LoadFile(t, tt.want.rspFile))
+		})
+	}
+}
